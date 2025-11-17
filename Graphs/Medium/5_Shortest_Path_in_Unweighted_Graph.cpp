522f6ec2d7b5332044ffe063f41e85668f4a3575
prashant-sagar-shakya <prashantshakya43602003@gmail.com>
2025-11-17 22:02:21 +0530

    Shortest Path in Unweighted Graph

--- Graphs/Medium/5_Shortest_Path_in_Unweighted_Graph.cpp
+++ Graphs/Medium/5_Shortest_Path_in_Unweighted_Graph.cpp
@@ -6,71 +6,49 @@ using namespace std;
     cin.tie(NULL);                    \
     cout.tie(NULL)
 
-// Classical Dijkstra's Algorithm for finding shortest paths from a single source
-void dijkstra(int n, vector<vector<pair<int, int>>> &adj, int src)
+int shortestPathBinaryMatrix(vector<vector<int>> &grid)
 {
-    // Distance array initialized with infinity
-    vector<int> dist(n, INT_MAX);
-    // Min-heap (priority queue) to get the node with the smallest distance
-    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
-    // Distance to source is 0
-    dist[src] = 0;
-    pq.push({0, src});
+    int n = grid.size();
 
-    // Process the graph
-    while (!pq.empty())
+    if (grid[0][0] == 1 || grid[n - 1][n - 1] == 1)
+        return -1;
+
+    vector<pair<int, int>> dirs = {
+        {-1, -1}, {-1, 0}, {-1, 1}, {0, -1}, {0, 1}, {1, -1}, {1, 0}, {1, 1}};
+    queue<vector<int>> q;
+    q.push({0, 0, 1});
+    grid[0][0] = 1;
+    while (!q.empty())
     {
-        int d = pq.top().first;     // current shortest distance
-        int node = pq.top().second; // current node
-        pq.pop();
-        // Skip if we already found a better path
-        if (d > dist[node])
-            continue;
-        // Explore all adjacent nodes
-        for (auto &edge : adj[node])
+        auto curr = q.front();
+        q.pop();
+        int r = curr[0], c = curr[1], dist = curr[2];
+        if (r == n - 1 && c == n - 1)
+            return dist;
+        for (auto &d : dirs)
         {
-            int next = edge.first;
-            int weight = edge.second;
-            // If a shorter path is found, update and push to the queue
-            if (dist[node] + weight < dist[next])
+            int nr = r + d.first;
+            int nc = c + d.second;
+            if (nr >= 0 && nr < n && nc >= 0 && nc < n && grid[nr][nc] == 0)
             {
-                dist[next] = dist[node] + weight;
-                pq.push({dist[next], next});
+                grid[nr][nc] = 1;
+                q.push({nr, nc, dist + 1});
             }
         }
     }
-
-    // Print shortest distances from source
-    cout << "Shortest distances from node " << src << ":\n";
-    for (int i = 0; i < n; ++i)
-    {
-        if (dist[i] == INT_MAX)
-            cout << "Node " << i << " : Unreachable\n";
-        else
-            cout << "Node " << i << " : " << dist[i] << "\n";
-    }
+    return -1;
 }
 
 // Your code here
 void Solve()
 {
-    int n = 5; // number of nodes
-    // Graph representation: adjacency list (node -> [(neighbor, weight)])
-    vector<vector<pair<int, int>>> adj(n);
-    // Sample undirected weighted graph
-    adj[0].push_back({1, 2});
-    adj[1].push_back({0, 2});
-    adj[0].push_back({2, 4});
-    adj[2].push_back({0, 4});
-    adj[1].push_back({2, 1});
-    adj[2].push_back({1, 1});
-    adj[1].push_back({3, 7});
-    adj[3].push_back({1, 7});
-    adj[2].push_back({4, 3});
-    adj[4].push_back({2, 3});
-
-    // Run Dijkstra from source node 0
-    dijkstra(n, adj, 0);
+    int n;
+    cin >> n;
+    vector<vector<int>> grid(n, vector<int>(n));
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+            cin >> grid[i][j];
+    cout << shortestPathBinaryMatrix(grid) << "\n";
 }
 
 // Driver code
